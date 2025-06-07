e4254e35e0bf4b1ba9b5cedc8fad02d8c7cc90f0
Strahinja Jankovic <strahinja.p.jankovic@gmail.com>
2025-06-07 17:20:21 +0200

    Update
    Signed-off-by: Strahinja Jankovic <strahinja.p.jankovic@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,61 @@
 //! ```
 //! cargo run --example unix-signal
 //! ```
-use signal_hook::{consts, low_level};
-use smol::{Async, prelude::*};
-use smol_macros::{Executor, main};
-use std::os::unix::{io::AsRawFd, net::UnixStream};
+use async_channel::{bounded, Receiver, Sender};
+use smol::{io, Async};
+use smol::{prelude::*, stream};
+use smol_macros::{main, Executor};
+use std::time::{Duration, Instant};
+use timerfd::{SetTimeFlags, TimerFd, TimerState};
+
+enum STest {
+    TEST1,
+    TEST2,
+}
 
 main! {
     async fn main(ex: &Executor<'_>) {
-        ex.spawn(async {
-            // Create a Unix stream that receives a byte on each signal occurrence.
-            let (a, mut b) = Async::<UnixStream>::pair().unwrap();
-            // Async isn't IntoRawFd, but it is AsRawFd, so let's pass the raw fd directly.
-            low_level::pipe::register_raw(consts::SIGINT, a.as_raw_fd()).unwrap();
-            println!("Waiting for Ctrl-C...");
+        let (sender, receiver) = bounded(10);
 
-            // Receive a byte that indicates the Ctrl-C signal occurred.
-            b.read_exact(&mut [0]).await.unwrap();
+        let t1 = ex.spawn(run_send(sender));
 
-            println!("Done!");
-        }).await;
+        let t2 = ex.spawn(run_receive(receiver));
+
+        let handles: Vec<smol::Task<()>> = vec![t1, t2];
+
+        for handle in handles {
+            handle.await;
+        }
     }
+}
+
+/// Sleeps using an OS timer.
+async fn sleep(dur: Duration) -> io::Result<()> {
+    // Create an OS timer.
+    let mut timer = TimerFd::new()?;
+    timer.set_state(TimerState::Oneshot(dur), SetTimeFlags::Default);
+
+    // When the OS timer fires, a 64-bit integer can be read from it.
+    Async::new(timer)?
+        .read_with(|t| rustix::io::read(t, &mut [0u8; 8]).map_err(io::Error::from))
+        .await?;
+    Ok(())
+}
+
+async fn run_receive(receiver: Receiver<STest>) {
+    let start = Instant::now();
+    println!("Sleeping...");
+    receiver.recv().await;
+
+    println!("Received after {:?}", start.elapsed());
+}
+async fn run_send(sender: Sender<STest>) {
+    let start = Instant::now();
+    println!("Sleeping...");
+
+    // Sleep for a second using an OS timer.
+    sleep(Duration::from_secs(1)).await.unwrap();
+
+    println!("Woke up after {:?}", start.elapsed());
+    sender.send(STest::TEST1).await;
 }
